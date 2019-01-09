50303f7fc6dd3d465b21a5b35414b0e85ee1da2f
pluz-stas <s.pluzhnick@student.csn.khai.edu>
2019-01-09 22:21:24 +0200

    HomeTask3

---
+++ HomeTask3GIT/HomeTask3GIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTask3GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HomeTask3");
+        }
+    }
+}
